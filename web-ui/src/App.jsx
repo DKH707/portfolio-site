24f44c41cd5218c7b50224ff6d7aaef882bb6c90
derek <derekkh707@gmail.com>
2024-06-10 17:30:23 -0500

    Initial site prototype

---
+++ web-ui/src/App.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+import './App.css'
+
+export default function App() {
+  return <>
+  <div className='bg-background'>
+    <Spline scene="https://prod.spline.design/gInKA-TXha8ySRF3/scene.splinecode" />
+  </div>
+  </>
+}
