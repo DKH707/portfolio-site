e23c586c9fb7dab17e027ee1ab2f80946c620ecd
derek <derekkh707@gmail.com>
2024-06-19 12:29:15 -0500

    Mobile view size

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 export default function App() {
   return <>
-  <div className='bg-background h-dvh'>
+  <div className='bg-background h-dvh sm:w-dvw'>
     <Spline scene="https://prod.spline.design/gInKA-TXha8ySRF3/scene.splinecode" />
   </div>
   </>
